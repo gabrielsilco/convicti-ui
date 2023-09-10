2e257486b24e4c9882b47ae9f3831fbafeb2d9b0
gabrielsilco <gabriel.silco@gmail.com>
2023-09-10 17:12:23 -0300

    Inserida a chamada para receber todas as empresas e renderizando de acordo com as coordenadas

--- ui/dev/src/pages/Index.vue
+++ ui/dev/src/pages/Index.vue
@@ -29,37 +29,81 @@
         <q-icon class="q-ml-md" color="primary" name="add" />
       </q-btn>
     </header>
-    <div id="map" style="width: 100%; height: 400px"></div>
+    <div class="map-container">
+      <div id="map" style="width: 100%; height: 600px"></div>
+    </div>
   </q-page>
 </template>
 
 <script>
 import axios from 'axios';
-import Map from 'ol/Map.js';
-import View from 'ol/View.js';
-import OSM from 'ol/source/OSM.js';
-import TileLayer from 'ol/layer/Tile.js';
+
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
+import Feature from 'ol/Feature';
+import Point from 'ol/geom/Point';
+import { fromLonLat } from 'ol/proj';
 
 export default {
+  data() {
+    return {
+      companies: [],
+      coordinatesArray: [],
+    }
+  },
   methods: {
     openAddCompanyDrawer() {
       this.$emit('openAddCompanyDrawer')
     }
   },
-  async created() {
+  async mounted() {
     const companies = await axios.get('http://localhost:8080/api/companies');
-    console.log(companies)
-  },
-  mounted() {
+    this.companies = companies.data.data;
+    this.coordinatesArray = this.companies.map(company => {
+      return [company.longitude, company.latitude]
+    })
+    console.log(this.coordinatesArray)
+
     const map = new Map({
+      target: 'map',
       layers: [
-        new TileLayer({source: new OSM()}),
+        new TileLayer({
+          source: new OSM(),
+        }),
+        // Add a vector layer for points
+        new VectorLayer({
+          source: new VectorSource(),
+        }),
       ],
       view: new View({
         center: [0, 0],
         zoom: 2,
       }),
-      target: 'map',
+    });
+
+    // Function to add points to the map
+    const addPoint = (coordinates) => {
+      const vectorSource = map.getLayers().item(1).getSource(); // Get the vector source
+      const point = new Point(fromLonLat(coordinates)); // Convert coordinates to the map's projection
+      const feature = new Feature(point);
+      vectorSource.addFeature(feature);
+    };
+
+    // Example: Add points based on coordinates
+    const coordinatesArray = [
+      ["-34.87696400", "-8.04756300"], // Example coordinates, replace with your own
+      // [10, 10], // Example coordinates, replace with your own
+      // Add more coordinates as needed
+    ];
+
+    await console.log(this.coordinatesArray)
+
+    this.coordinatesArray.forEach((coordinates) => {
+      addPoint(coordinates);
     });
   }
 }
@@ -85,4 +129,7 @@ export default {
   font-weight: 600
   letter-spacing: .8px
   line-height: 0
+
+.map-container
+  margin-top: 24px
 </style>
